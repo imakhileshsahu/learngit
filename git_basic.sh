547d2d15f6fa95edf5a3c7d3464a94f4b43166bc
Akhilesh sahu <akhileshsahu@Akhileshs-Mac.local>
2023-09-03 11:37:53 +0530

    add more cmd

--- git_basic.sh
+++ git_basic.sh
@@ -5,3 +5,4 @@ git commit -m "your message"
 git log     // git commit logs 
 git show  232343234 // show change of commit
 git diff  // show stage file difference 
+git revert 23492234 // get old changes
