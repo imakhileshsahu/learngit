2fee3db527b709570438fdb9751cbb10e94229bf
Akhilesh sahu <akhileshsahu@Akhileshs-Mac.local>
2023-09-03 11:35:19 +0530

    add basic git cmd

---
+++ git_basic.sh
@@ -0,0 +1,7 @@
+git init   /repository inititialisation
+git add .   / add file into git 
+git status 
+git commit -m "your message"
+git log     // git commit logs 
+git show  232343234 // show change of commit
+git diff  // show stage file difference 
